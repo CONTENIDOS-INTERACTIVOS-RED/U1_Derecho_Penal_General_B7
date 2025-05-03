5be77dec869062a1defff21a2a342b8b39003ac0
devcontenidosinteractivos <200440498+devcontenidosinteractivos@users.noreply.github.com>
2025-05-03 08:49:17 -0500

    Initial commit
    Se realiza home, intro, sintesis, json

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,8 @@
 export default {
   global: {
-    numeroUnidad: '3',
-    tituloUnidad: 'Bioética y sostenibilidad en el desarrollo económico global',
+    numeroUnidad: '1',
+    tituloUnidad:
+      'El poder punitivo del estado. Evolución histórica del derecho penal. Escuelas penales.',
   },
   menuPrincipal: {
     menu: [
@@ -15,19 +16,19 @@ export default {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Título tema',
+        titulo: 'El poder punitivo del Estado y sus límites',
         desarrolloContenidos: true,
         subMenu: [
           {
             icono: 'far fa-file-alt',
             numero: '1.1',
-            titulo: 'Subtítulo tema',
+            titulo: 'Concepto y justificación del poder punitivo',
             hash: 't_1_1',
           },
           {
             icono: 'far fa-file-alt',
             numero: '1.2',
-            titulo: 'Subtítulo tema',
+            titulo: 'Principios que limitan el poder punitivo del Estado',
             hash: 't_1_2',
           },
         ],
@@ -36,47 +37,26 @@ export default {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Título tema',
+        titulo: 'Evolución histórica del Derecho Penal',
         desarrolloContenidos: true,
         subMenu: [
           {
             icono: 'far fa-file-alt',
             numero: '2.1',
-            titulo: 'Subtítulo tema',
+            titulo: 'Orígenes del Derecho Penal y primeros códigos normativos',
             hash: 't_2_1',
           },
           {
             icono: 'far fa-file-alt',
             numero: '2.2',
-            titulo: 'Subtítulo tema',
+            titulo: 'Derecho Penal Romano Germánico',
             hash: 't_2_2',
           },
           {
             icono: 'far fa-file-alt',
             numero: '2.3',
-            titulo: 'Subtítulo tema',
+            titulo: 'Derecho penal en la edad moderna y contemporánea',
             hash: 't_2_3',
-          },
-        ],
-      },
-      {
-        nombreRuta: 'tema3',
-        icono: 'far fa-file-alt',
-        numero: '3',
-        titulo: 'Título tema',
-        desarrolloContenidos: true,
-        subMenu: [
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.1',
-            titulo: 'Subtítulo tema',
-            hash: 't_3_1',
-          },
-          {
-            icono: 'far fa-file-alt',
-            numero: '3.2',
-            titulo: 'Subtítulo tema',
-            hash: 't_3_2',
           },
         ],
       },
@@ -118,26 +98,96 @@ export default {
   referencias: [
     {
       referencia:
-        'Ardaraz, M. (2002). Teoría de las tres dimensiones de desarrollo sostenible: . Ecosistemas, 11(2)',
-      link:
-        'https://www.revistaecosistemas.net/index.php/ecosistemas/article/view/614',
+        'Casanueva Sainz, I. (2013). Derecho Penal. Parte general: materiales didácticos: (3 ed.). Publicaciones de la Universidad de Deusto. ',
+      link: 'https://elibro.net/es/lc/tecnologicadeloriente/titulos/34064 ',
     },
     {
       referencia:
-        'Beltrán, D. J. (2002). La estrategia de desarrollo sostenible de la Unión Europea en el contexto global: de Río a Johannesburgo. ICE. Revista de Economía, (800).',
-      link: '',
+        'Muñoz Clares, J. (2013). Determinaciones preliminares del derecho penal: ( ed.). Difusora Larousse - Editorial Tecnos. ',
+      link: 'https://elibro.net/es/lc/tecnologicadeloriente/titulos/114269 ',
+    },
+    {
+      referencia:
+        'Del Valle, C. P. (2008). Estudios sobre los fundamentos del derecho penal: ( ed.). Dykinson. ',
+      link:
+        'https://elibro.net/es/ereader/tecnologicadeloriente/61036?page=10 ',
+    },
+    {
+      referencia:
+        'López Betancourt, E. (2018). Derecho procesal penal: (3 ed.). IURE Editores. ',
+      link: 'https://elibro.net/es/lc/tecnologicadeloriente/titulos/40253 ',
+    },
+    {
+      referencia:
+        'Hernández Quintero, H. A. (2007). Cuadernos de Derecho Penal Económico: ( ed.). Universidad de Ibagué. ',
+      link: 'https://elibro.net/es/lc/tecnologicadeloriente/titulos/70752 ',
+    },
+    {
+      referencia:
+        'López Betancourt, E. (2018). Derecho procesal penal: (3 ed.). IURE Editores. ',
+      link: 'https://elibro.net/es/lc/tecnologicadeloriente/titulos/40253 ',
     },
   ],
   glosario: [
     {
-      termino: 'Bioética',
+      termino: 'Control social penal',
       significado:
-        'disciplina que estudia las implicaciones éticas, sociales y legales de los avances biológicos y médicos, buscando garantizar que las decisiones en estos campos respeten la dignidad humana, los derechos fundamentales y la justicia social.',
+        'mecanismo mediante el cual el Estado regula conductas a través de normas y sanciones en el marco del Derecho Penal.',
     },
     {
-      termino: '<em>Design Thinking</em>',
+      termino: 'Debido proceso',
       significado:
-        'método de diseño centrado en el usuario que se basa en la comprensión de las necesidades y expectativas de los clientes, para crear soluciones innovadoras y efectivas.',
+        'conjunto de garantías procesales que aseguran un juicio justo e imparcial para cualquier persona acusada de un delito.',
+    },
+    {
+      termino: 'Delito',
+      significado:
+        'conducta tipificada en la ley como punible, que afecta bienes jurídicos protegidos por el Estado.',
+    },
+    {
+      termino: '<em>Lus puniendi</em>',
+      significado:
+        'facultad exclusiva del Estado para definir y aplicar sanciones penales a quienes infringen el orden jurídico.',
+    },
+    {
+      termino: 'Pena',
+      significado:
+        'sanción impuesta por el Estado a quienes cometen delitos, con el fin de castigar y prevenir conductas ilícitas.',
+    },
+    {
+      termino: 'Presunción de inocencia',
+      significado:
+        'derecho fundamental que establece que toda persona es inocente hasta que se pruebe su culpabilidad.',
+    },
+    {
+      termino: 'Principio de culpabilidad',
+      significado:
+        'garantía penal que establece que solo se puede sancionar a quien haya accionado con dolo o culpa.',
+    },
+    {
+      termino: 'Principio de intervención mínima',
+      significado:
+        'considera el Derecho Penal como la última instancia para conductas regulares, impidiendo su uso excesivo.',
+    },
+    {
+      termino: 'Principio de legalidad',
+      significado:
+        'ninguna persona puede ser sancionada por una conducta que no esté previamente tipificada como delito.',
+    },
+    {
+      termino: 'Principio de proporcionalidad',
+      significado:
+        'la pena impuesta debe ser adecuada y proporcional a la gravedad del delito cometido.',
+    },
+    {
+      termino: 'Principio del acto',
+      significado:
+        'solo pueden sancionarse conductas humanas concretas, no meras intenciones o pensamientos del individuo.',
+    },
+    {
+      termino: 'Ultima ratio',
+      significado:
+        'idea de que el Derecho Penal debe ser utilizado como último recurso ante la imposibilidad de otras soluciones jurídicas.',
     },
   ],
 }
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -3,4 +3,27 @@
   BannerInterno(subTitulo="Introducción")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
 
+    .row.mb-5
+      .col-lg-4.order-2
+        figure
+          img(src="@/assets/curso/intro/1.png", data-aos="zoom-in")
+      .col-lg-8.order-1.mb-3.mb-lg-0
+        .bg-color-1.p-4.mb-3(data-aos="fade-left")
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/intro/2.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 El Derecho Penal es una de las ramas fundamentales del ordenamiento jurídico, encargada de regular la potestad punitiva del Estado y definir las conductas que se consideran delictivas, así como las sanciones penales correspondientes. Su evolución ha estado determinada por las necesidades de cada sociedad a lo largo del tiempo, reflejando cambios en la concepción de la justicia, el castigo y la prevención del delito. Comprender la historia y los fundamentos del Derecho Penal permite analizar su impacto en la regulación de la convivencia social y la protección de los bienes jurídicos tutelados.
+
+        p(data-aos="fade-left") En esta unidad se estudiará el poder punitivo del Estado y su desarrollo histórico, examinando las principales corrientes de pensamiento que han influido en la configuración del Derecho Penal. A partir del estudio de sistemas normativos antiguos, como el Código de Hammurabi, la Ley Mosaica y el Código de Manú, será posible comprender los primeros intentos de establecer reglas para el control del delito y el mantenimiento del orden social. Asimismo, se examinará la evolución del Derecho Penal en el mundo occidental, con especial énfasis en el Derecho Penal Romano Germánico y en las diversas escuelas que han marcado su desarrollo, como la Escuela Positiva, la Escuela Neoclásica, la Escuela Funcionalista y la Escuela Finalista
+    
+    .bg-full-width.bg-color-2
+      .px-4.px-md-5.py-4
+        .row.align-items-center
+          .col-lg-3
+            img(src="@/assets/curso/intro/3.svg", data-aos="zoom-in").mx-auto
+          .col-lg-9
+            p.mb-0.text-white El propósito de esta unidad es proporcionar una base conceptual sólida que permita identificar los principios fundamentales del Derecho Penal y su evolución como ciencia jurídica. Al finalizar el estudio, se espera que se logre un análisis crítico de la potestad punitiva del Estado y sus implicaciones en la formulación de normas penales. Asimismo, la indagación de las diferentes corrientes doctrinales permitirá contrastar sus enfoques y valorar su impacto en la interpretación y aplicación del Derecho Penal en la actualidad.
+    
+        
 </template>
